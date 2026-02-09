228ba2a1350a6eff3b9ab3a6c79a7d7921a70890
CkalaiGit <cairedine@gmail.com>
2026-02-09 01:14:46 +0100

    feat(auth): ajouter service d'authentification et interceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthService } from './auth/auth.service';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: `
-    <div style="padding: 20px; font-family: sans-serif;">
-      <h1>Finance App - Module 1</h1>
-      
-      <div *ngIf="!auth.isLoggedIn">
-        <button (click)="auth.login()">Se connecter avec Keycloak</button>
-      </div>
-
-      <div *ngIf="auth.isLoggedIn">
-        <p>Bonjour <strong>{{ auth.username }}</strong> !</p>
-        <button (click)="auth.logout()">Déconnexion</button>
-      </div>
-    </div>
-  `
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   public auth = inject(AuthService);
